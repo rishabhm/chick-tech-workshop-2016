88a5417d2e2829dc898f7c405720910ade409be2
rishabhmarya <user@users-MacBook-Pro.local>
2016-12-02 02:24:06 -0800

    dynamic ports and start script

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(request, response) {
   response.send('Hello world!');
 });
 
-app.listen(3000, function() {
-  console.log('Node app is running on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
 
